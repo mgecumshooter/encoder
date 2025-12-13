5907a63d9f6e8a1dd9015d92f8ea55ba1d3c9a44
mgecumshooter <barlipro@mail.ru>
2025-12-13 20:58:51 +0300

    Fixed previous bugs; Added special symbols support for encoded/decoded strings; Added CLI functionality;

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include <stack>
 #include <cmath>
 #include <locale>
-// #include <codecvt>
 // There's only standart library :sunglasses:
 using namespace std;
 
@@ -106,21 +105,22 @@ void doKey(wstring c, wstring& key){
 // // This shit takes the string and the key and encodes it
 wstring encode(wstring word, wstring key, string alph){
     doKey(word, key);
-    wstring enc;
-	wcout << "\n\t" << word << "\n";
-	wcout << "\n\t" << key << "\n";
-	cout << "\n\t" << alph << "\n";
+	wstring enc;
 	if (alph == "en"){
 		for (int i = 0; i < word.length(); i++){
-			if (isCap(word[i], alph)){
+			if (alphPos(word[i], alph) == -1){
+				enc += word[i];
+			}else if (isCap(word[i], alph)){
 				enc += capAlph_en[(alphPos(word[i], alph) + alphPos(key[i], alph)) % 27];
 			}else if (!isCap(word[i], alph)){
 				enc += alph_en[(alphPos(word[i], alph) + alphPos(key[i], alph)) % 27];
 			}
 		}
 	}else if (alph == "ru"){
-		for (int i = 0; i <= word.length(); i++){
-			if (isCap(word[i], alph) == true){
+		for (int i = 0; i < word.length(); i++){
+			if (alphPos(word[i], alph) == -1){
+				enc += word[i];
+			}else if (isCap(word[i], alph) == true){
 				enc += capAlph_ru[(alphPos(word[i], alph) + alphPos(key[i], alph)) % 33];
 			}else if (isCap(word[i], alph) == false){
 				enc += alph_ru[(alphPos(word[i], alph) + alphPos(key[i], alph)) % 33];
@@ -135,33 +135,37 @@ wstring decode(wstring word, wstring key, string alph){
     wstring dec;
 	if (alph == "en"){
 		for (int i = 0; i != word.length(); i++){
-			if (alphPos(word[i], alph) >= alphPos(key[i], alph)){
+			if (alphPos(word[i], alph) == -1){
+				dec += word[i];
+			}else if (alphPos(word[i], alph) >= alphPos(key[i], alph)){
 				if (isCap(word[i], alph)){
 					dec += capAlph_en[(alphPos(word[i], alph) - alphPos(key[i], alph)) % 27];
 				}else if (!isCap(word[i], alph)){
 					dec += alph_en[(alphPos(word[i], alph) - alphPos(key[i], alph)) % 27];
 				}
-			}else {
+			}else if (alphPos(word[i], alph) < alphPos(key[i], alph)){
 				if (isCap(word[i], alph)){
-					dec += capAlph_en[(27 - alphPos(word[i], alph) - alphPos(key[i], alph)) % 27];
+					dec += capAlph_en[(27 - (alphPos(key[i], alph) - alphPos(word[i], alph))) % 27];
 				}else if (!isCap(word[i], alph)){
-					dec += alph_en[(27 - alphPos(word[i], alph) - alphPos(key[i], alph)) % 27];
+					dec += alph_en[(27 - (alphPos(key[i], alph) - alphPos(word[i], alph))) % 27];
 				}
 			}
 		}
 	}else if (alph == "ru"){
 		for (int i = 0; i != word.length(); i++){
-			if (alphPos(word[i], alph) >= alphPos(key[i], alph)){
+			if (alphPos(word[i], alph) == -1){
+				dec += word[i];
+			}else if (alphPos(word[i], alph) >= alphPos(key[i], alph)){
 				if (isCap(word[i], alph) == true){
 					dec += capAlph_ru[(alphPos(word[i], alph) - alphPos(key[i], alph)) % 33];
 				}else if (isCap(word[i], alph) == false){
 					dec += alph_ru[(alphPos(word[i], alph) - alphPos(key[i], alph)) % 33];
 				}
-			}else {
+			}else if(alphPos(word[i], alph) < alphPos(key[i], alph)){
 				if (isCap(word[i], alph) == true){
-					dec += capAlph_ru[(33 - alphPos(word[i], alph) - alphPos(key[i], alph)) % 33];
+					dec += capAlph_ru[(33 - (alphPos(key[i], alph) - alphPos(word[i], alph))) % 33];
 				}else if (isCap(word[i], alph) == false){
-					dec += alph_ru[(33 - alphPos(word[i], alph) - alphPos(key[i], alph)) % 33];
+					dec += alph_ru[(33 - (alphPos(key[i], alph) - alphPos(word[i], alph))) % 33];
 				}
 			}
 		}
@@ -169,50 +173,51 @@ wstring decode(wstring word, wstring key, string alph){
     return dec;
 }
 
-// // This Shit takes encoded string and dekodes it
-// string decode(string word, string key){
-//     string dec;
-//     toLowerCase(word);
-//     toLowerCase(key);
-//     doKey(word, key);
-//     for (int i = 0; i != word.length(); i++){
-//         if (alphPos(word[i]) >= alphPos(key[i])){
-//             dec += ALPHABET[(alphPos(word[i]) - alphPos(key[i])) % 27];
-//         } else {
-//             dec += ALPHABET[(27 - (alphPos(key[i]) - alphPos(word[i]))) % 27];
-//         }
-//     }
-//     return dec;
-// }
-
-// void encode (char c, char key){
-
-// }
-
-// string alph(char c){
-//     if (c == ' '){
-//         return "en";
-//     }else {
-//         for (char b: ALPHABET){
-//             if (b == c){
-//                 return "en";
-//             }else if (alphPos(c, "en") >= 26 && c != b){
-//                 return "ru";
-//             }
-//         }
-//     }
-// }
-
 // This shit does the main shit. shit.
 int main(){
 	locale::global(locale("ru_RU.UTF-8"));
     wcin.imbue(locale());
     wcout.imbue(locale());
 
-	string alphb = "ru";
-	wstring word = L"пенис";
-	wstring key = L"хуй";
-	wstring penis = encode(word, key, alphb);
-	wcout << encode(word, key, alphb) << endl;
-	wcout << decode(penis , key, alphb) << endl;
+	int Mode = 0;
+	int UserAlph = 0;
+	wstring StUserAlph;
+	wstring StMode;
+	string Alph;
+	wstring Word;
+	wstring Key;
+
+	wcout << "Welcome to the ENCODER!" << endl;
+	wcout << "Select mode [Encode - 0; Decode - 1] (default - Encode):\n\t";
+	getline(wcin, StMode);
+	if (StMode == L""){}
+	else { Mode = stoi(StMode); }
+	wcout << "Select alphabet [EN - 0; RU - 1] (default - EN):\n\t";
+	getline(wcin, StUserAlph);
+	if (StUserAlph == L""){}
+	else { UserAlph = stoi(StUserAlph); }
+	wcout << "Type your text to operate on (one string):\n\t";
+	getline(wcin, Word);
+	wcout << "Type your key (only alphabet letters and space):\n\t";
+	getline(wcin, Key);
+
+	switch (UserAlph){
+		case 0:
+			Alph = "en";
+			break;
+		case 1:
+			Alph = "ru";
+			break;
+		default:
+			Alph = "en";
+	}
+
+	switch (Mode){
+		case 1:
+			wcout << "Your decoded string:\n\t" << decode(Word, Key, Alph) << endl;
+			break;
+		default:
+			wcout << "Your encoded string:\n\t" << encode(Word, Key, Alph) << endl;
+			break;
+	}
 }
